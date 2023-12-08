7faed3cf552271bc9b4f164d09e15346d5584ab1
Aleks B <aleksandir@me.com>
2023-12-09 10:04:40 +1030

    Add dependencies and implement Spotify API integration

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/aleksandir/Sheetmusic-renamer
 
 go 1.16
 
-require github.com/joho/godotenv v1.5.1 // indirect
+require (
+	github.com/joho/godotenv v1.5.1 // indirect
+	github.com/zmb3/spotify/v2 v2.4.0 // indirect
+)
--- src/main.go
+++ src/main.go
@@ -1,7 +1,57 @@
 package main
 
-import "fmt"
+import (
+	"encoding/base64"
+	"fmt"
+	"net/http"
+	"net/url"
+	"os"
+	"strings"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Load .env file
+	err := godotenv.Load("src/.env")
+	if err != nil {
+		fmt.Println("Error loading .env file")
+		return
+	}
+
+	clientID := os.Getenv("SPOTIFY_CLIENT_ID")
+	clientSecret := os.Getenv("SPOTIFY_CLIENT_SECRET")
+
+	// Base64 encode the client ID and secret
+	auth := base64.StdEncoding.EncodeToString([]byte(clientID + ":" + clientSecret))
+
+	data := url.Values{}
+	data.Set("grant_type", "client_credentials")
+
+	req, err := http.NewRequest("POST", "", strings.NewReader(data.Encode()))
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+
+	req.Header.Add("Authorization", "Basic "+auth)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	client := &http.Client{}
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	// handle response
+	fmt.Println("Response:", string(resp.Status))
 }
+
+// func getToken() string {
+// 	authString := fmt.Sprintf("%v:%v", os.Getenv("CLIENT_ID"), os.Getenv("CLIENT_SECRET"))
+// 	encodedAuthString := base64.StdEncoding.EncodeToString([]byte(authString))
+
+// 	return encodedAuthString
+// }
