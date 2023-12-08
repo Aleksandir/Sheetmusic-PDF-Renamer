d253a7c5988e9d34c2ee87663754801e949ca079
Aleks B <aleksandir@me.com>
2023-12-08 22:37:19 +1030

    Refactor code to remove starter code from copilot new
    will start over from the ground up.

--- src/main.go
+++ src/main.go
@@ -1,48 +1,7 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"os"
-	"path/filepath"
-	"strings"
-
-	"github.com/aleksandir/Sheetmusic-renamer/src/spotifyapi"
-)
+import "fmt"
 
 func main() {
-	// Get the directory path from the command line arguments
-	// dirPath := os.Args[1]
-	//! for testing
-	dirPath := "testFiles"
-
-	// Get all PDF files in the directory
-	pdfFiles, err := filepath.Glob(filepath.Join(dirPath, "*.pdf"))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// For each PDF file
-	for _, oldPath := range pdfFiles {
-		// Get the old name without the extension
-		oldName := strings.TrimSuffix(filepath.Base(oldPath), filepath.Ext(oldPath))
-
-		newName, name2, err := spotifyapi.GetSongAndArtist(oldName)
-		fmt.Println(oldName, newName, name2, err)
-		if err != nil {
-			log.Println("Error getting song and artist:", err)
-			continue
-		}
-
-		// Get the new path
-		newPath := filepath.Join(filepath.Dir(oldPath), newName+".pdf")
-
-		err = os.Rename(oldPath, newPath)
-		if err != nil {
-			log.Println("Error renaming file:", err)
-			continue
-		}
-
-		log.Println("Renamed", oldPath, "to", newPath)
-	}
+	fmt.Println("Hello, World!")
 }
