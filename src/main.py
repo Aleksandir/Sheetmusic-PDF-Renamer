6f08289bcb7a2004e17e3ea5e113a8125f3c4768
Aleks B <aleksandir@me.com>
2023-12-09 18:20:22 +1030

    Add musicbrainz.py module for title and artist retrieval

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 import os
 import string
 
-import requests
+from musicbrainz import get_title_and_artist
 
 # URL for testing: https://musicbrainz.org/ws/2/release?limit=1&query=Chasing-Cars-Part&fmt=json
 
@@ -127,38 +127,6 @@ def scan_dir(dir):
         index += 1
 
     return new_names, differences
-
-
-# current bugs
-# 1 sometimes returns artist as song title (Iris-The-Goo-Goo-Dolls-Peter-John-Arrangeme... => Goo Goo Dolls - Goo Goo Dolls) & (Poison - Alice-Cooper => Alice Cooper - Alice Cooper)
-# 2 sometimes gets the artist wrong and instead has another artist or uses a cover artist (creep chords => Creep - VELIAL SQUAD, Stairway to heven => Heven - O Saala Sakraal)
-def get_title_and_artist(song):
-    response = requests.get(
-        f"https://musicbrainz.org/ws/2/release?limit=1&query={song}&fmt=json"
-    )
-
-    # Check if the request was successful
-    if response.status_code == 200:
-        data = response.json()
-
-        # Check if there are any recordings in the response
-        if data["releases"]:
-            first_recording = data["releases"][0]
-            title = first_recording["title"]
-
-            # Check if there are any artist credits in the first recording
-            if first_recording["artist-credit"]:
-                artist = first_recording["artist-credit"][0]["name"]
-            else:
-                artist = "No artist credit found."
-        else:
-            title = "No recordings found."
-            artist = "No recordings found."
-    else:
-        title = "Error"
-        artist = "Error"
-
-    return title, artist
 
 
 main()
---
+++ src/musicbrainz.py
@@ -0,0 +1,34 @@
+import requests
+
+# current bugs
+# 1 sometimes returns artist as song title (Iris-The-Goo-Goo-Dolls-Peter-John-Arrangeme... => Goo Goo Dolls - Goo Goo Dolls) & (Poison - Alice-Cooper => Alice Cooper - Alice Cooper)
+# 2 sometimes gets the artist wrong and instead has another artist or uses a cover artist (creep chords => Creep - VELIAL SQUAD, Stairway to heven => Heven - O Saala Sakraal)
+
+
+def get_title_and_artist(song):
+    response = requests.get(
+        f"https://musicbrainz.org/ws/2/release?limit=1&query={song}&fmt=json"
+    )
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+
+        # Check if there are any recordings in the response
+        if data["releases"]:
+            first_recording = data["releases"][0]
+            title = first_recording["title"]
+
+            # Check if there are any artist credits in the first recording
+            if first_recording["artist-credit"]:
+                artist = first_recording["artist-credit"][0]["name"]
+            else:
+                artist = "No artist credit found."
+        else:
+            title = "No recordings found."
+            artist = "No recordings found."
+    else:
+        title = "Error"
+        artist = "Error"
+
+    return title, artist
