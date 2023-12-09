1149af04c01ae230a947c83da877de5c24348b79
Aleks B <aleksandir@me.com>
2023-12-09 15:38:59 +1030

    Add functionality to retrieve title and artist from MusicBrainz API

--- src/main.py
+++ src/main.py
@@ -1,26 +1,47 @@
+import os
+
 import requests
 
-# URL for testing: https://musicbrainz.org/ws/2/release?'limit=1&query=Chasing-Cars-Part&fmt=json
-song = "Chasing-Cars-Part"  # replace with your song title
-response = requests.get(
-    f"https://musicbrainz.org/ws/2/release?'limit=1&query={song}&fmt=json"
-)
+# URL for testing: https://musicbrainz.org/ws/2/release?limit=1&query=Chasing-Cars-Part&fmt=json
 
-# Check if the request was successful
-if response.status_code == 200:
-    data = response.json()
 
-    # Check if there are any recordings in the response
-    if data["releases"]:
-        first_recording = data["releases"][0]
-        print("Title:", first_recording["title"])
+testFiles_dir = "testFiles"  # replace with your directory
 
-        # Check if there are any artist credits in the first recording
-        if first_recording["artist-credit"]:
-            print("Artist:", first_recording["artist-credit"][0]["name"])
+
+def main():
+    for filename in os.listdir(testFiles_dir):
+        base_name, ext = os.path.splitext(filename)
+        title, artist = get_title_and_artist(base_name)
+        print(f"Base name: {base_name}\n{title} - {artist}\n")
+
+
+def get_title_and_artist(song):
+    response = requests.get(
+        f"https://musicbrainz.org/ws/2/release?limit=1&query={song}&fmt=json"
+    )
+
+    # Check if the request was successful
+    if response.status_code == 200:
+        data = response.json()
+
+        # Check if there are any recordings in the response
+        if data["releases"]:
+            first_recording = data["releases"][0]
+            title = first_recording["title"]
+
+            # Check if there are any artist credits in the first recording
+            if first_recording["artist-credit"]:
+                artist = first_recording["artist-credit"][0]["name"]
+            else:
+                artist = "No artist credit found."
         else:
-            print("No artist credit found.")
+            title = "No recordings found."
+            artist = "No recordings found."
     else:
-        print("No recordings found.")
-else:
-    print("Error:", response.status_code)
+        title = "Error"
+        artist = "Error"
+
+    return title, artist
+
+
+main()
