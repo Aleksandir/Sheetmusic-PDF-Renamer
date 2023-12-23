ba3bb971b8c6fb27c51ce7572299b27029b78ac6
Aleks B <aleksandir@me.com>
2023-12-23 17:06:55 +1030

    Refactor get_title_and_artist function signature

--- src/lastfm_API.py
+++ src/lastfm_API.py
@@ -4,7 +4,7 @@ import requests
 from dotenv import load_dotenv
 
 
-def get_title_and_artist(song):
+def get_title_and_artist(song: str) -> tuple[str, str]:
     load_dotenv()
 
     APIKEY = os.getenv("LASTFM_API_KEY")
@@ -13,7 +13,9 @@ def get_title_and_artist(song):
         f"http://ws.audioscrobbler.com/2.0/?method=track.search&limit=1&track={song}&api_key={APIKEY}&format=json"
     )
 
-    # Check if the request was successful
+    title = "Error"
+    artist = "Error"
+
     if response.status_code == 200:
         data = response.json()
 
@@ -21,8 +23,5 @@ def get_title_and_artist(song):
         if data["results"]["trackmatches"]["track"]:
             title = data["results"]["trackmatches"]["track"][0]["name"]
             artist = data["results"]["trackmatches"]["track"][0]["artist"]
-        else:
-            title = "Error"
-            artist = "Error"
 
     return title, artist
